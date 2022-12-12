3f72ced2de52825e8e7b5b4ba7a3aa95d8f8ab3d
hardikthecoder <85208662+hardiktheprogrammer@users.noreply.github.com>
2022-12-12 08:56:09 +0530

    Update Files

--- BlockdevNFT's/src/components/Header.js
+++ BlockdevNFT's/src/components/Header.js
@@ -14,7 +14,7 @@ const Header = () => {
 
     return (
         <div id="header">
-        <Link to='/' id='logo'>NFT Room</Link>
+        <Link to='/' id='logo'>BlockdevNFT's</Link>
 
         <div id="link-containers">
           <a>Start Hunting</a>
